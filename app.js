875840eef21ad5f7ee6d2e13ac083cf01833fd13
Ivan Navarrete <crzivn@gmail.com>
2019-10-05 22:35:33 +0200

    Make graphs data-driven

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const locale = "us";
 
+let outerRadius = null;
+let innerRadius = null;
+let stackGraphsRadius = null;
+let stackGraphWidth = null;
+
+let holidays = [];
+let data = [];
+
 window.onload = async () => {
   const svg = d3.select("svg");
   const margin = { top: 50, bottom: 50, left: 50, right: 50 };
   const w = +svg.attr("width") - margin.left - margin.right;
   const h = +svg.attr("height") - margin.top - margin.bottom;
 
-  const outerRadius = Math.min(w, h) / 2;
-  const innerRadius = Math.floor(outerRadius / 4 + 5);
-  const stackGraphsRadius = innerRadius * 2;
-  const stackGraphWidth = (stackGraphsRadius - innerRadius) / 5;
+  outerRadius = Math.min(w, h) / 2;
+  innerRadius = Math.floor(outerRadius / 4 + 5);
+  stackGraphsRadius = innerRadius * 2 + 20;
+  stackGraphWidth = (stackGraphsRadius - innerRadius) / 5;
 
-  const holidays  = await d3.csv("data/holidays.csv", (d) => parseDate(d.Date).getTime());
-  const data      = await d3.csv("data/air_quality.csv", (d) => ({
+  holidays  = await d3.csv("data/holidays.csv", (d) => parseDate(d.Date).getTime());
+  data      = await d3.csv("data/air_quality.csv", (d) => ({
       Date: parseDate(d.Date),
       DateRaw: parseDate(d.Date).getTime(),
       TEMP: parseFloat(d.Temp),
@@ -24,8 +32,29 @@ window.onload = async () => {
     })
   );
 
-  let gMain = svg.append("g")
+
+  const g = svg.append("g")
     .attr("transform", `translate(${w / 2 + margin.left}, ${outerRadius + margin.top})`);
+
+  g.append("g").attr("class", "x-axis");
+  g.append("g").attr("class", "y-axis");
+
+  g.append("g").attr("class", "graphs")
+    .selectAll("g")
+    .data(["co", "no2", "so2", "pm10", "pm2_5", "temp"])
+    .join("g")
+      .attr("class", d => `${d}-graph`)
+
+  g.select(".temp-graph").append("g").attr("class", "graph");
+
+  g.append("g").attr("class", "tooltips")
+  g.append("text").attr("class", "legend")
+
+  draw(data, holidays);
+}
+
+const draw = (data, holidays) => {
+  const g = d3.select("svg g");
 
   const xStep = Math.PI * 2 / data.length;
 
@@ -36,22 +65,22 @@ window.onload = async () => {
 
   const y = d3.scaleLinear()
     .domain(d3.extent(data.map(d => d.TEMP)))
-    .range([innerRadius + stackGraphWidth * 5, outerRadius]);
+    .range([innerRadius + stackGraphWidth * 5, outerRadius])
+    .nice();
 
-  drawXAxis(gMain, data, holidays, x, innerRadius, outerRadius);
-  drawYAxis(gMain, y);
+  const _gTooltips = d3.select(".tooltips").selectAll("g")
+    .data(data, d => d.Date)
 
-  drawLegend(gMain, data);
+  _gTooltips.exit().remove()
 
-  const _gTooltips = d3.create("svg:g")
-    .attr("class", "tooltips")
-
-  const gTooltips = _gTooltips.selectAll("g")
-    .data(data)
+  const gTooltips = _gTooltips
     .enter().append("g")
-      .attr("class", d => `tooltip-${d.DateRaw}`)
+      .attr("class", d => `tooltip-${d.DateRaw}`);
 
-  gGraphs = gMain.append("g").attr("class", "graphs");
+  drawXAxis(data, holidays, x, innerRadius, outerRadius);
+  drawYAxis(data, y);
+  drawTempGraph(data, x, y);
+  drawLegend(data);
 
   [["co",4],["no2",80],["so2",150],["pm10",150],["pm2_5",75]].forEach(([name, standard], i) => {
     const _innerRadius = innerRadius + stackGraphWidth * i;
@@ -61,21 +90,17 @@ window.onload = async () => {
       .domain([0, d3.max(data, d => d[name.toUpperCase()])])
       .range([_outerRadius, _innerRadius])
 
-    drawStackGraph(name, data, _innerRadius, stackGraphWidth, x, y, gGraphs);
-    drawStackYAxis(name, standard, y, gGraphs);
-    drawTooltips(name, data, x, () => (_innerRadius + stackGraphWidth / 2), gTooltips);
+    drawStackGraph(name, data, x, y);
+    drawStackYAxis(name, standard, y);
+    drawTooltips(data, x, () => (_innerRadius + stackGraphWidth / 2), name, gTooltips);
   });
 
-  const gTempGraph = gGraphs.append("g").attr("class", "temp-graph")
-  drawTempGraph(gTempGraph, data, x, y);
-  drawTooltips("temp", data, x, y, gTooltips);
+  drawTooltips(data, x, y, "temp", gTooltips);
 
-  gMain.append(() => _gTooltips.node());
-
-  bindEvents(data, x, y, () => drawTempGraph(gMain, data, x, y));
+  bindEvents(data, x, y);
 }
 
-const drawTooltips = (name, data, x, y, g) => {
+const drawTooltips = (data, x, y, name, g) => {
   const accessor = name.toUpperCase();
 
   g.append("text")
@@ -87,196 +112,194 @@ const drawTooltips = (name, data, x, y, g) => {
     .text(d => d[accessor]);
 }
 
-const parseDate = d3.timeParse("%Y-%m-%d");
-
-const formatDate = (d) => (
-  d.Date.toLocaleDateString(locale, { day: "numeric", month: "short" })
-)
-
-const rotateText = (d, x) => (
-  (x(d.DateRaw) + x.bandwidth() / 2 - Math.PI * 0.5) * 180 / Math.PI
-);
-
-const drawStackGraph = (name, data, innerRadius, width, x, y, g) => {
+const drawStackGraph = (name, data, x, y) => {
   const accessor = name.toUpperCase();
 
   const [min, max] = y.domain();
   const step = (max - min) / 5;
 
-  const arc = d3.arc()
-    .innerRadius(innerRadius + width)
+  const graph = d3.arc()
+    .innerRadius(d => y(0))
     .outerRadius(d => y(d[accessor]))
     .startAngle(d => x(d.DateRaw))
     .endAngle(d => x(d.DateRaw) + x.bandwidth());
 
-  const arcLevels = (n) => {
-    const level = (d) => d3.max([y(step * n), y(d[accessor])])
+  const _data = data.map(d => [d, d]).reduce((a, v) => a.concat(v), []);
 
-    return d3.arc()
-      .innerRadius(d => level(d))
-      .outerRadius(d => level(d))
-      .startAngle(d => x(d.DateRaw))
-      .endAngle(d => x(d.DateRaw) + x.bandwidth())
-  };
+  const _x = d3.scaleLinear()
+    .domain([0, _data.length])
+    .range(x.range())
 
-  const line = d3.lineRadial()
-    .angle(d => x(d.DateRaw))
-    .radius(d => y(d[accessor]))
-    .curve(d3.curveStepAfter)
+  const levels = (n) => (
+    d3.lineRadial()
+      .radius(d => y(step * n))
+      .angle((d, i) => _x(i % 2 === 1 ? i + 1: i))
+      .defined(d => y(d[accessor]) < y(step * n))
+  )
 
-  const graph = g.append("g")
-      .attr("class", `${name}-graph`)
-
-  graph.selectAll("path")
-    .data(data)
-    .join("g")
-      .call(g => g.append("path")
-        .attr("class", "levels")
-        .attr("d", arcLevels(1)))
-      .call(g => g.append("path")
-        .attr("class", "levels")
-        .attr("d", arcLevels(2)))
-      .call(g => g.append("path")
-        .attr("class", "levels")
-        .attr("d", arcLevels(3)))
-      .call(g => g.append("path")
-        .attr("class", "levels")
-        .attr("d", arcLevels(4)))
-      .call(g => g.append("path")
-        .attr("class", "values")
-        .attr("d", arc))
-
-  graph.append("g")
-      .attr("class", "y-axis")
-    .append("circle")
-      .attr("class", "ytick ytick-inner")
-      .attr("r", innerRadius)
+  d3.select(`.${name}-graph`).selectAll(".graph")
+    .data([_data])
+    .join(
+      enter => enter.append("g")
+        .attr("class", "graph")
+        .call(g => {
+          g.append("path").attr("class", "level")
+          g.append("path").attr("class", "level")
+          g.append("path").attr("class", "level")
+          g.append("path").attr("class", "level")
+      })
+    )
+    .call(g => {
+      g.select(".level:nth-child(1)").attr("d", levels(1))
+      g.select(".level:nth-child(2)").attr("d", levels(2))
+      g.select(".level:nth-child(3)").attr("d", levels(3))
+      g.select(".level:nth-child(4)").attr("d", levels(4))
+    })
+    .call(g =>
+      g.selectAll(".value")
+        .data(data)
+        .join(
+          enter => enter.append("path")
+            .attr("class", "value")
+        )
+        .attr("d", graph)
+    )
 }
 
-const drawXAxis = (g, data, holidays, x, innerRadius, outerRadius) => {
+const drawXAxis = (data, holidays, x, innerRadius, outerRadius) => {
   const hitBox = d3.arc()
-      .innerRadius(innerRadius)
-      .outerRadius(outerRadius)
-      .startAngle(d => x(d.DateRaw))
-      .endAngle(d => x(d.DateRaw) + x.bandwidth())
-
-  const xAxis = g.append("g")
-      .attr("class", "x-axis");
-
-  const xTick = xAxis.selectAll("g")
-    .data(data)
-    .enter().append("g")
-
-  xTick.append("path")
-      .attr("class", "hitbox")
-      .attr("d", hitBox);
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius)
+    .startAngle(d => x(d.DateRaw))
+    .endAngle(d => x(d.DateRaw) + x.bandwidth())
 
   const labelMargin = 10;
   const inRightHalf = (i) => (i < (data.length / 2));
 
-  xTick.append("text")
+  d3.select(".x-axis").selectAll("g")
+    .data(data)
+    .join(
+      enter => enter.append("g")
+        .call(g => g.append("path").attr("class", "hitbox"))
+        .call(g => g.append("text")),
+    )
+    .call(g => g.select("path").attr("d", hitBox))
+    .call(g => g.select("text")
       .classed("holiday", d => holidays.includes(d.DateRaw))
-      .attr("text-anchor", (_, i) => inRightHalf(i) ? "start" : "end")
+      .attr("text-anchor", (d, i) => inRightHalf(i) ? "start" : "end")
       .attr("transform", (d, i) => (`
          rotate(${rotateText(d, x)})
          translate(${outerRadius + labelMargin}, 0)
          rotate(${inRightHalf(i) ? 0 : 180})
       `))
-      .text(d => formatDate(d));
+      .text(d => formatDate(d.Date))
+    );
 }
 
-const drawStackYAxis = (name, standard, y, g) => {
-  const pollutantAxis = g.append("g")
-    .attr("class", "y-axis");
+const drawStackYAxis = (name, standard, y) => {
+  d3.select(`.${name}-graph`).selectAll(".y-axis")
+    .data([name])
+    .join(
+      enter => enter.append("g")
+        .attr("class", "y-axis")
+        .call(g => {
+          g.append("circle")
+            .attr("class", "ytick ytick-inner")
+            .attr("r", d3.min(y.range()));
 
-  pollutantAxis.append("circle")
-    .attr("class", "pollutant-standard")
-    .attr("r", y(standard));
+          g.append("circle")
+            .attr("class", "pollutant-standard")
+            .attr("r", y(standard));
 
-  pollutantAxis.append("text")
-    .attr("class", name)
-    .attr("y", -y(standard))
-    .attr("dy", "0.35em")
-    .text(standard);
+          g.append("text")
+            .attr("class", name)
+            .attr("y", -y(standard))
+            .attr("dy", "0.35em")
+            .text(standard);
+        })
+    );
 }
 
-const drawYAxis = (g, y) => {
-  const yLevels = 10;
-  const [innerRadius, outerRadius] = y.range();
+const drawYAxis = (data, y) => {
+  const yTicks = y.ticks(10);
 
-  const yTick = d3.lineRadial()
-      .angle(d => x(d.DateRaw) + x.bandwidth() / 2)
-      .radius(d => y(d))
+  d3.select(".y-axis").selectAll("g")
+    .data(yTicks)
+    .join(
+      enter => enter.append("g")
+        .call(g => {
+          g.append("circle")
+            .attr("class", "ytick")
+            .classed("ytick-inner", (d, i) => i === 0)
+            .classed("ytick-outer", (d, i) => i === yTicks.length - 1)
+            .classed("ytick-center", d     => d === 0)
+            .attr("r", y)
 
-  const yAxis = g.append("g")
-    .attr("class", "y-axis");
-
-  yAxis.append("circle")
-      .attr("class", "ytick ytick-inner")
-      .attr("r", innerRadius);
-
-  yAxis.call(g => g.selectAll("g")
-    .data(y.ticks(yLevels))
-    //.data([-20, -15, -10, -5, 0, 5, 10, 15])
-    .join("g")
-      .call(g => g.append("circle")
-          .attr("class", "ytick")
-          .classed("ytick-center", d => d === 0)
-          .attr("r", y))
-      .call(g => g.append("text")
-          .attr("class", "temp")
-          .attr("y", d => -y(d))
-          .attr("dy", "0.35em")
-          .text(d => d)))
-
-  yAxis.append("circle")
-      .attr("class", "ytick ytick-outer")
-      .attr("r", outerRadius);
+          g.append("text")
+            .attr("class", "temp")
+            .attr("y", d => -y(d))
+            .attr("dy", "0.35em")
+            .text((d, i) => (i > 0 && i < yTicks.length - 1) ? d : "")
+        })
+    );
 }
 
-const drawLegend = (g, data) => {
+const drawLegend = (data) => {
   const [start, end] = d3.extent(data, d => d.Date);
 
-  const startYear = start.getFullYear();
-  const endYear = end.getFullYear();
-  const startMonth = start.toLocaleDateString(locale, { month: "short" });
-  const endMonth = end.toLocaleDateString(locale, { month: "short" });
+  const dateFormat = {year: "numeric", day: "numeric", month: "short" };
+  const [_start, _end] = [formatDate(start, dateFormat), formatDate(end, dateFormat)];
 
-  const yearText = startYear === endYear ? startYear : `${startYear}   ${endYear}`;
-  const monthText = `${startMonth} - ${endMonth}`;
+  const legend = d3.select(".legend");
+  legend.selectAll("tspan").remove();
 
-  const legend = g.append("text")
-      .attr("class", "legend")
+  let monthText = "";
+  let yearText = "";
+
+  if (_start === _end) {
+    yearText = start.getFullYear();
+    monthText = formatDate(start, { day: "numeric", month: "short" });
+
+  } else {
+    const startYear = start.getFullYear();
+    const endYear = end.getFullYear();
+    const startMonth = formatDate(start, { month: "short" });
+    const endMonth = formatDate(end, { month: "short" });
+
+    monthText = `${startMonth} - ${endMonth}`;
+    yearText = startYear === endYear ? startYear : `${startYear}   ${endYear}`;
+  }
 
   legend.append("tspan")
-      .attr("class", "month")
-      .text(monthText);
+    .attr("class", "month")
+    .text(monthText);
 
   legend.append("tspan")
-      .attr("class", "year")
-      .attr("x", 0)
-      .attr("dy", "1.2em")
-      .text(yearText);
+    .attr("class", "year")
+    .attr("x", 0)
+    .attr("dy", "1.2em")
+    .text(yearText);
 }
 
-const drawTempGraph = (g, data, x, y) => {
-  const [innerRadius, _] = y.range();
-
+const drawTempGraph = (data, x, y) => {
   const graph = d3.areaRadial()
-      .angle(d => x(d.DateRaw) + x.bandwidth() / 2)
-      .innerRadius(y(0))
-      .outerRadius(d => y(d.TEMP))
-      .curve(d3.curveCatmullRom)
+    .angle(d => x(d.DateRaw) + x.bandwidth() / 2)
+    .innerRadius(y(0))
+    .outerRadius(d => y(d.TEMP))
+    .curve(d3.curveCatmullRom)
 
-  const lineGraph = g.append("path")
-      .datum(data)
-      .attr("d", graph);
+  d3.select(".temp-graph .graph").selectAll("path")
+    .data([data])
+    .join(
+      enter => enter.append("path")
+    )
+    .attr("d", graph);
 }
 
-const bindEvents = (data, x, y, redraw) => {
+const bindEvents = (data, x, y) => {
   d3.selectAll(".x-axis .hitbox")
     .on("mouseenter", function(d) {
-      d3.select(this.parentElement).raise();
+      d3.select(this.parentNode).raise();
 
       d3.selectAll(`.tooltip-${d.DateRaw}`)
         .classed("show", true)
@@ -286,3 +309,14 @@ const bindEvents = (data, x, y, redraw) => {
         .classed("show", false)
     })
 }
+
+const parseDate = d3.timeParse("%Y-%m-%d");
+const parseTime = d3.timeParse("%H:%M:%S");
+
+const formatDate = (date, format = null) => (
+  date.toLocaleDateString(locale, format || { day: "numeric", month: "short" })
+)
+
+const rotateText = (d, x) => (
+  (x(d.DateRaw) + x.bandwidth() / 2 - Math.PI * 0.5) * 180 / Math.PI
+);
